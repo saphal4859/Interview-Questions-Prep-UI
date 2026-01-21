e4acc8e8cf58f8422099b78eb726ac363862ef2e
saphal4859 <saisaphal123@gmail.com>
2026-01-22 00:13:13 +0530

    dashboard

--- src/services/dashboardApi.js
+++ src/services/dashboardApi.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const fetchDashboard = async () => {
     // import.meta.env.VITE_API_BASE_URL
-  const res = await axios.get("https://interview-questions-prep.onrender.com/api/dashboard/overview");
+  const res = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/api/dashboard/overview`);
   return res.data;
 };
